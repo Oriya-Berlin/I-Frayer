dc5595fefecc3973b36f3e540d43fc9a94f6125b
Your Name <oriya2699@gmail.com>
2020-07-27 01:41:59 +0300

    first commit

--- Final_Project/settings.py
+++ Final_Project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k$n@=n998to@*0_=(!!uct)um%n2dd7nh4ui#z8l^8414anvw^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
